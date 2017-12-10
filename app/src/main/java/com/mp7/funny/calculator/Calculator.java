e87a7cec599c874717a99261d75280230499f4bc
dao3 <31672051+dao3@users.noreply.github.com>
2017-12-09 22:03:02 -0600

    yayyy

--- app/src/main/java/com/mp7/funny/calculator/Calculator.java
+++ app/src/main/java/com/mp7/funny/calculator/Calculator.java
@@ -2,12 +2,51 @@ package com.mp7.funny.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Calculator extends AppCompatActivity {
-
+    TextView displayBinary, displayDecimal;
+    Button one, zero, clear;
+    int input, output;
+    String inputString, outputString;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+    input = 0;
+    inputString = "";
+    outputString = "";
+    one =  findViewById(R.id.button1);
+    zero =  findViewById(R.id.button0);
+    clear =  findViewById(R.id.clear);
+    displayBinary =  findViewById(R.id.displayBinary);
+    displayDecimal = findViewById(R.id.displayDecimal);
+    one.setOnClickListener(new View.OnClickListener(){
+        public void onClick(View v) {
+            inputString = inputString + "1";
+            input = Integer.parseInt(inputString,2);
+            outputString = Integer.toString(input);
+        }
+    });
+    zero.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            inputString = inputString + "0";
+            input = Integer.parseInt(inputString,2);
+            outputString = Integer.toString(input);
+        }
+    });
+    clear.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            input = 0b0;
+            output = 0b0;
+            inputString = "";
+            outputString = "";
+        }
+    });
     }
+
 }
